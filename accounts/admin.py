d6ebbb331d253f44a64530a1b955df5604deb894
ZapdasMYF <yassar.farooq@zapdas.co>
2020-05-03 05:50:28 +0500

    reset password forgot password change password

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,3 +2,8 @@ from django.contrib import admin
 from algo.models import Feedback,ContactForm
 from django.contrib.auth.models import auth,User
 # Register your models here.
+
+admin.site.site_header = "TrendsHub"
+admin.site.site_title = "TrendsHub Admin Portal"
+admin.site.index_title = "Welcome to TrendsHub Portal"
+
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,32 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    path('login', views.login, name='login'),
+    path('login', views.login, name='signin'),
     path('register', views.register, name='register'),
     path("logout" ,views.logout, name="logout"),
+
+    #url('email-verify/(?P<token>[\w\-]+)',register.confirm_email),
+    #url('resend-confirmation/(?P<email>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})',register.resend_confirmation_email),
+
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
+                                    name='password_change_done'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_reset_form.html'),
+                                    name='password_change'),
+
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
+                                    name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+                                    name='password_reset_complete'),
+
+    #url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    #url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    #url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #auth_views.password_reset_confirm, name='password_reset_confirm'),
+    #url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
